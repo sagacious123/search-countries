12f00b19fbce120ed2162c41b456e34e8abefa1c
bajegbo joseph <damilolaj23@gmail.com>
2021-07-22 16:19:12 +0100

    new adjustments to the details page

--- src/Components/CountryDetails.js
+++ src/Components/CountryDetails.js
@@ -15,14 +15,50 @@ const CountryDetails = ({ getCountry, newCountries }) => {
   // })
 
   return (
-    <div>
-      <Link to="/" className="px-3 py-2 shadow-md rounded-sm">
-        Go back
-      </Link>
-      <div>{getCountry.name}</div>
-      <img className="w-16" src="image.jpg" />
-      <h2>details</h2>
-      {/* <div>{ncCountries}</div> */}
+    <div className="w-11/12 max-w-7xl m-auto">
+      <div>
+        <Link to="/" className="px-3 py-2 shadow-md rounded-sm">
+          <i className="fa fa-arrow-back mr-2"></i>
+          Go back
+        </Link>
+      </div>
+      <div className="flex md:items-center items-start justify-between md:flex-row flex-col m-auto max-w-97 md:max-w-full w-5/8 md:full">
+        <div className="md:w-6/8 w-full">
+          <img src={getCountry.flag} alt={getCountry.name} className="w-full h-full" />
+        </div>
+        <div className="lg:w-5/12 md:w-6/12 w-full">
+          <h1>{getCountry.name}</h1>
+          <div className="flex items-start justify-between md:flex-row flex-col">
+            <div>
+              <p><span>Native Name:</span> {getCountry.nativeName}</p>
+              <p><span>Population:</span> {getCountry.population}</p>
+              <p><span>Region:</span> {getCountry.region}</p>
+              <p><span>Sub Region:</span> {getCountry.subregion}</p>
+              <p><span>Capital:</span> {getCountry.capital}</p>
+            </div>
+            <div>
+              <p><span>Top Level Domain:</span> {getCountry.TopLevelDomain}</p>
+              {getCountry.currencies.map((currency) => {
+                return <p><span>Currencies:</span> {currency.name}</p>
+              })}
+              {getCountry.currencies.map((currency) => {
+                return <p><span>Languages:</span> {getCountry.languages.name}</p>
+              })}
+            </div>
+          </div>
+          <div>
+            <p>Border Countries:</p>
+            <div>
+              {getCountry.borders.map((border) => {
+                return <span>{border}</span>
+              })}
+              
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
